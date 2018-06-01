ab5cb4a3a08f09bd1cced520dbadc829131abd15
hinet <63603636@qq.com>
2018-06-01 15:53:08 +0800

    Update demo.vue

--- demo/demo.vue
+++ demo/demo.vue
@@ -1,1 +1,17 @@
 
+<template>
+<datetime format="YYYY年MM月DD日 H时i分" v-model="val"></datetime>
+</template>
+<script>
+    import datetime from '../src/datetime';
+    export default {
+      data(){
+        return {
+          val:null
+        }
+      },
+      components:{
+        datetime
+      }
+    }
+</script>
